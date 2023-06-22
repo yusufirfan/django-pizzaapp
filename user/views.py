84ffcfff41651cd8de1d3b6ffadf38e854d4b3ab
Yusuf <kurakiorg@gmail.com>
2023-06-22 23:13:19 +0300

    redirect path

--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
 
+REDIRECT_URL = 'user_login'
+
 class FixView:
     template_name = 'user/user_form.html'
-    next_page = 'user_login'
+    next_page = REDIRECT_URL
 
 class UserLoginView(FixView, LoginView):
     pass
@@ -15,7 +17,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateView(FixView, CreateView):
     form_class = UserCreationForm
-    success_url = '/user/login/'
+    # success_url = '/user/login/'
+    success_url = REDIRECT_URL
 
     def get_success_url(self):
         from django.contrib.auth import login
