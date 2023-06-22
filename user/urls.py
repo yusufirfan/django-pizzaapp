442c2100b5b7779eac77819ff5bf9b9e8cf9c707
Yusuf <kurakiorg@gmail.com>
2023-06-22 21:58:28 +0300

    Updates

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='user_login'),
+    path('logout/', UserLogoutView.as_view(), name='user_logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+class UserLoginView(LoginView):
+    template_name = 'user/user_form.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'user/user_form.html'
