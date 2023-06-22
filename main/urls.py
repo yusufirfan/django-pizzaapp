bf3bf7441301037df903d598a72f6be4a90966be
Yusuf <kurakiorg@gmail.com>
2023-06-22 20:47:21 +0300

    User urls, forms and main urls

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user', include('user.urls')),
 ]
 
 from django.conf import settings
