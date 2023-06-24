a48891e54b69d25ca112be2ec0dafdd082b7524b
Yusuf <kurakiorg@gmail.com>
2023-06-24 12:05:11 +0300

    Pizza views done

--- pizza/views.py
+++ pizza/views.py
@@ -7,3 +7,44 @@ def home(request):
         'pizzas' : pizzas
     }
     return render(request, 'home.html', context)
+
+from django.contrib import messages
+from django.urls import reverse_lazy
+
+from .forms import (
+    Order, OrderForm
+)
+
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+
+
+class FixView:
+    model = Order
+    form_class = OrderForm
+    success_url = reverse_lazy('order_list')
+
+class OrderListView(FixView, ListView):
+    template_name = 'pizza/order_list.html'
+    context_object_name = 'orders'
+    order = ['-id']
+
+class OrderCreateView(FixView, CreateView):
+    template_name = 'pizza/order_form.html'
+    context_object_name = 'form'
+
+class OrderDetailView(FixView, DetailView):
+    template_name = 'pizza/order_detail.html'
+    context_object_name = 'form'
+
+class OrderUpdateView(FixView, UpdateView):
+    template_name = 'pizza/order_form.html'
+    context_object_name = 'form'
+
+class OrderDeleteView(FixView, DeleteView)
+    pass
